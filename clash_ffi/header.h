c096060cd912defb913c0330a5a7c272cc95c4d3
dev0 <dev@watfaq.com>
2023-09-20 18:49:14 +1000

    ffi

--- clash_ffi/header.h
+++ clash_ffi/header.h
@@ -1,11 +1,6 @@
-#include <cstdarg>
-#include <cstdint>
-#include <cstdlib>
-#include <ostream>
-#include <new>
-
-extern "C" {
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 void start_clash(const char *cfg_str);
-
-} // extern "C"
