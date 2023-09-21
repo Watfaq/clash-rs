e7b6fda724b39facb448cbab1078028b1a0a45a0
dev0 <dev@watfaq.com>
2023-09-22 04:18:23 +1000

    arm

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,6 @@ set -xe
 for TARGET in $1; do
   echo "building for $TARGET"
   rustup target add $TARGET
-  case $TARGET in
-  aarch64-unknown-linux-gnu)
-    sudo apt-get install -y gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu
-    export BINDGEN_EXTRA_CLANG_ARGS='-I/usr/aarch64-linux-gnu/include'
-      ;;
-  *)
-      ;;
-  esac
   cargo build -p clash --target $TARGET --release
   ls -l ./target/$TARGET/release/
   mv ./target/$TARGET/release/clash ./target/artifacts/clash-$TARGET
