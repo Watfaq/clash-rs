72ebd297811c195001a76fa9e5cbd54906956103
dev0 <dev@watfaq.com>
2023-11-06 14:50:46 +1100

    f wq

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -xe
 [ ! -d "target/artifacts" ] && mkdir -p target/artifacts
 
 for TARGET in $1; do
-  TARGET=$TARGET | tr -d '[:space:]' | tr -d '\n' | tr -d '\r'
+  TARGET=$(echo -n $TARGET | tr -d '[:space:]' | tr -d '\n' | tr -d '\r')
   echo "building for $TARGET"
   rustup target add $TARGET
   cargo build -p clash --target $TARGET --release
