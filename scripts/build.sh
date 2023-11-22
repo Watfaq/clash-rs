5600f92ae08f12f60b6deba1236ff42bd4d34ed2
dev0 <dev@watfaq.com>
2023-11-23 04:40:55 +1100

    up

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,9 @@ set -xe
 
 [ ! -d "target/artifacts" ] && mkdir -p target/artifacts
 
+sudo apt update
+sudo apt install -y gcc-multilib gcc make
+
 for TARGET in $1; do
   TARGET=`echo "$TARGET" | tr -d '[:space:]' | tr -d '\n' | tr -d '\r'`
   echo "building for $TARGET"
@@ -11,18 +14,18 @@ for TARGET in $1; do
 
   case $TARGET in
     aarch64-unknown-linux-gnu)
-      sudo apt install -y gcc make gcc-aarch64-linux-gnu g++-aarch64-linux-gnu  binutils-aarch64-linux-gnu 
+      sudo apt install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu binutils-aarch64-linux-gnu libc6-dev-arm64-cross
       export CC=aarch64-linux-gnu-gcc
       export CXX=aarch64-linux-gnu-g++
       export BINDGEN_EXTRA_CLANG_ARGS="--sysroot=/usr/aarch64-linux-gnu"
       ;;
     arm-unknown-linux-gnueabi)
-      sudo apt install -y gcc make gcc-arm-linux-gnueabi g++-arm-linux-gnueabi binutils-arm-linux-gnueabi
+      sudo apt install -y gcc-arm-linux-gnueabi g++-arm-linux-gnueabi binutils-arm-linux-gnueabi libc6-dev-armel-cross
       export CC=arm-linux-gnueabi-gcc
       export CXX=arm-linux-gnueabi-g++
       ;;
     armv7-unknown-linux-gnueabihf)
-      sudo apt install -y gcc make gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf binutils-arm-linux-gnueabihf
+      sudo apt install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf binutils-arm-linux-gnueabihf libc6-dev-armhf-cross
       export CC=arm-linux-gnueabihf-gcc
       export CXX=arm-linux-gnueabihf-g++
       ;;
