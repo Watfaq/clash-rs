37fc1f0f3b6ca5bf798bba18e0146548a78302f7
Yuwei B <i@xiaoba.me>
2024-05-10 16:14:14 +1000

    use legacy image

--- clash_lib/src/proxy/utils/test_utils/consts.rs
+++ clash_lib/src/proxy/utils/test_utils/consts.rs
@@ -1,6 +1,6 @@
 pub const LOCAL_ADDR: &str = "127.0.0.1";
 
-pub const IMAGE_WG: &str = "lscr.io/linuxserver/wireguard:latest";
+pub const IMAGE_WG: &str = "lscr.io/linuxserver/wireguard:1.0.20210914-legacy";
 pub const IMAGE_SS_RUST: &str = "ghcr.io/shadowsocks/ssserver-rust:latest";
 pub const IMAGE_SHADOW_TLS: &str = "ghcr.io/ihciah/shadow-tls:latest";
 pub const IMAGE_TROJAN_GO: &str = "p4gefau1t/trojan-go:latest";
