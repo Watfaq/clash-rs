8c0f8df46d2a07cf17b914a312c63ba84b2d5f5d
Yuwei Ba <dev@watfaq.com>
2024-08-02 16:26:05 +1000

    feat(api): implement some control APIs (#524)
    * mem usage
    
    * DNS
    
    * restart
    
    * clippy
    
    * clippy
    
    * clippy
    
    * clippy

--- clash_lib/src/app/api/handlers/mod.rs
+++ clash_lib/src/app/api/handlers/mod.rs
@@ -3,9 +3,12 @@ pub mod connection;
 pub mod dns;
 pub mod hello;
 pub mod log;
+pub mod memory;
 pub mod provider;
 pub mod proxy;
+pub mod restart;
 pub mod rule;
 pub mod traffic;
-mod utils;
 pub mod version;
+
+mod utils;
