4ee9a7b59cafcf79bc39cff4b4b4669496424946
dev0 <dev@watfaq.com>
2024-08-14 01:16:55 +1000

    clippy

--- clash_lib/src/proxy/utils/platform/unix.rs
+++ clash_lib/src/proxy/utils/platform/unix.rs
@@ -5,7 +5,7 @@ use crate::proxy::utils::Interface;
 pub(crate) fn must_bind_socket_on_interface(
     socket: &socket2::Socket,
     iface: &Interface,
-    family: socket2::Domain,
+    _family: socket2::Domain,
 ) -> io::Result<()> {
     match iface {
         Interface::IpAddr(ip) => socket.bind(&SocketAddr::new(*ip, 0).into()),
