e9db8049acc204804c3b9a89a566bfd9fb1454d0
dev0 <dev@watfaq.com>
2024-08-14 01:22:11 +1000

    darwin

--- clash_lib/src/proxy/utils/platform/mod.rs
+++ clash_lib/src/proxy/utils/platform/mod.rs
@@ -1,5 +1,7 @@
 #[cfg(target_vendor = "apple")]
 mod apple;
+#[cfg(target_vendor = "apple")]
+pub(crate) use apple::must_bind_socket_on_interface;
 #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
 pub(crate) mod unix;
 #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
