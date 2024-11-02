71984cb495ada70d2e46d8599fc4879f8f08f9de
Yuwei Ba <dev@watfaq.com>
2024-11-03 01:38:02 +1100

    fix build on other plats (#636)

--- clash_lib/src/proxy/tun/routes/other.rs
+++ clash_lib/src/proxy/tun/routes/other.rs
@@ -1,9 +1,17 @@
 use ipnet::IpNet;
 use tracing::warn;
 
-use crate::proxy::utils::OutboundInterface;
+use crate::{config::internal::config::TunConfig, proxy::utils::OutboundInterface};
 
 pub fn add_route(_: &OutboundInterface, _: &IpNet) -> std::io::Result<()> {
     warn!("add_route is not implemented on {}", std::env::consts::OS);
+    Ok(())
+}
+
+pub fn maybe_routes_clean_up(_: &TunConfig) -> std::io::Result<()> {
+    warn!(
+        "maybe_routes_clean_up is not implemented on {}",
+        std::env::consts::OS
+    );
     Ok(())
 }
