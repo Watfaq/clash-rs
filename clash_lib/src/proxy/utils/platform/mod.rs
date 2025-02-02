3ec29245c0957277a5eacacee70d6e03c43f768c
lakako <example@example.com>
2025-02-03 00:26:00 +0800

    fix build on freebsd

--- clash_lib/src/proxy/utils/platform/mod.rs
+++ clash_lib/src/proxy/utils/platform/mod.rs
@@ -3,9 +3,13 @@ mod apple;
 #[cfg(target_vendor = "apple")]
 pub(crate) use apple::must_bind_socket_on_interface;
 #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
-pub(crate) mod unix;
+pub(crate) mod linux;
 #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
-pub(crate) use unix::must_bind_socket_on_interface;
+pub(crate) use linux::must_bind_socket_on_interface;
+#[cfg(target_os = "freebsd")]
+pub(crate) mod freebsd;
+#[cfg(target_os = "freebsd")]
+pub(crate) use freebsd::must_bind_socket_on_interface;
 #[cfg(windows)]
 pub(crate) mod win;
 #[cfg(windows)]
