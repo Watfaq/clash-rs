56785a87d3c681266d9b36ae3590d0e4478824ee
dev0 <dev@watfaq.com>
2025-02-13 05:17:26 +1100

    up

--- clash_lib/src/proxy/utils/platform/unix.rs
+++ clash_lib/src/proxy/utils/platform/unix.rs
@@ -9,6 +9,24 @@ pub(crate) fn must_bind_socket_on_interface(
 ) -> io::Result<()> {
     match iface {
         Interface::IpAddr(ip) => socket.bind(&SocketAddr::new(*ip, 0).into()),
-        Interface::Name(name) => socket.bind_device(Some(name.as_bytes())),
+        Interface::Name(name) => {
+            #[cfg(any(
+                target_os = "android",
+                target_os = "fuchsia",
+                target_os = "linux",
+            ))]
+            {
+                return socket.bind_device(Some(name.as_bytes()));
+            }
+            #[cfg(not(any(
+                target_os = "android",
+                target_os = "fuchsia",
+                target_os = "linux",
+            )))]
+            {
+                use crate::common::errors::new_io_error;
+                Err(new_io_error(format!("unsupported platform: {}", name)))
+            }
+        }
     }
 }
