701202f59a5b31b2c4f1336bb3a6030c5b3c68a0
dev0 <dev@watfaq.com>
2025-07-15 12:51:05 +1000

    f

--- clash-lib/src/proxy/utils/platform/unix.rs
+++ clash-lib/src/proxy/utils/platform/unix.rs
@@ -5,7 +5,41 @@ use crate::app::net::OutboundInterface;
 pub(crate) fn must_bind_socket_on_interface(
     socket: &socket2::Socket,
     iface: &OutboundInterface,
-    #[allow(unused)] family: socket2::Domain,
+    family: socket2::Domain,
 ) -> io::Result<()> {
-    socket.bind_device(Some(iface.name.as_bytes()))
+    #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux",))]
+    {
+        if iface.index == 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "OutboundInterface index is 0, cannot bind to interface",
+            ));
+        }
+        match family {
+            socket2::Domain::IPV4 => {
+                socket.bind_device_by_index_v4(Some(iface.index))?
+            }
+            socket2::Domain::IPV6 => {
+                socket.bind_device_by_index_v6(Some(iface.index))?
+            }
+            _ => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "unsupported address family",
+                ));
+            }
+        }
+    }
+    #[cfg(not(any(
+        target_os = "android",
+        target_os = "fuchsia",
+        target_os = "linux",
+    )))]
+    {
+        use crate::common::errors::new_io_error;
+        Err(new_io_error(format!(
+            "unsupported platform: {}",
+            iface.name
+        )))
+    }
 }
