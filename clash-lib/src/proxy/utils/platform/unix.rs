f8669bfb2a0938ec2f2e83ad379403996fe84ba1
dev0 <dev@watfaq.com>
2025-07-15 11:27:08 +1000

    f

--- clash-lib/src/proxy/utils/platform/unix.rs
+++ clash-lib/src/proxy/utils/platform/unix.rs
@@ -2,26 +2,10 @@ use std::io;
 
 use crate::app::net::OutboundInterface;
 
-#[allow(dead_code)]
 pub(crate) fn must_bind_socket_on_interface(
-    #[allow(unused)] socket: &socket2::Socket,
+    socket: &socket2::Socket,
     iface: &OutboundInterface,
     #[allow(unused)] family: socket2::Domain,
 ) -> io::Result<()> {
-    #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux",))]
-    {
-        socket.bind_device(Some(iface.name.as_bytes()))
-    }
-    #[cfg(not(any(
-        target_os = "android",
-        target_os = "fuchsia",
-        target_os = "linux",
-    )))]
-    {
-        use crate::common::errors::new_io_error;
-        Err(new_io_error(format!(
-            "unsupported platform: {}",
-            iface.name
-        )))
-    }
+    socket.bind_device(Some(iface.name.as_bytes()))
 }
