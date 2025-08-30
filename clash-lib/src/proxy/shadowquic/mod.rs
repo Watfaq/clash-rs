3b83a057801de261f1932679adde143658bdc3bb
spongebob888 <121923937+spongebob888@users.noreply.github.com>
2025-08-30 17:23:35 +0800

    fix(proxy): udp not bound for shadowquic #918 (#919)

--- clash-lib/src/proxy/shadowquic/mod.rs
+++ clash-lib/src/proxy/shadowquic/mod.rs
@@ -88,12 +88,17 @@ impl Handler {
                     }
                     SocksAddr::Ip(socket_addr) => socket_addr,
                 };
+                let bind_addr = if addr.is_ipv4() {
+                    "0.0.0.0:0".parse().unwrap()
+                } else {
+                    "[::]:0".parse().unwrap()
+                };
                 let socket = new_udp_socket(
-                    None,
+                    Some(bind_addr),
                     sess.iface.as_ref(),
                     #[cfg(target_os = "linux")]
                     sess.so_mark,
-                    Some(addr),
+                    None,
                 )
                 .await?;
 
