8c54223f141a1ceb53a4b83823ce938311c32694
dev0 <dev@watfaq.com>
2025-11-02 00:05:22 +1100

    match process name

--- clash-lib/src/app/router/rules/process.rs
+++ clash-lib/src/app/router/rules/process.rs
@@ -1,4 +1,5 @@
 use super::RuleMatcher;
+use crate::session::Network;
 
 pub struct Process {
     pub name: String,
@@ -14,9 +15,35 @@ impl std::fmt::Display for Process {
 }
 
 impl RuleMatcher for Process {
-    fn apply(&self, _sess: &crate::session::Session) -> bool {
-        // TODO: implement this
-        false
+    fn apply(&self, sess: &crate::session::Session) -> bool {
+        #[cfg(not(windows))]
+        {
+            use tracing::debug;
+
+            sock2proc::find_process_name(
+                Some(sess.source),
+                sess.destination.clone().try_into_socket_addr(),
+                match sess.network {
+                    Network::Tcp => sock2proc::NetworkProtocol::TCP,
+                    Network::Udp => sock2proc::NetworkProtocol::UDP,
+                },
+            )
+            .is_some_and(|proc| {
+                debug!("Matching process name: {} with {}", proc, self.name);
+                if self.name_only {
+                    proc == self.name
+                } else {
+                    proc.contains(&self.name)
+                }
+            })
+        }
+        #[cfg(windows)]
+        {
+            use tracing::info;
+
+            info!("PROCESS-NAME not supported on Windows yet");
+            false
+        }
     }
 
     fn target(&self) -> &str {
