9e6642c76e8d0629c73ede16fc0f24bcff064856
dev0 <dev@watfaq.com>
2025-11-03 18:18:53 +1100

    up

--- clash-lib/src/app/router/rules/process.rs
+++ clash-lib/src/app/router/rules/process.rs
@@ -41,7 +41,7 @@ impl RuleMatcher for Process {
         {
             use tracing::info;
 
-            info!(sess = &sess, "PROCESS-NAME not supported on Windows yet");
+            info!("PROCESS-NAME not supported on Windows yet: {}", &sess);
             false
         }
     }
